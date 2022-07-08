708930ddd93d89cc2826898ca357dc122a600710
Martin Milev <marto379@abv.bg>
2022-07-08 19:58:40 +0300

    Add

---
+++ Person/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        string name;
+        int age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+           
+        }
+
+        public string Name
+        {
+
+            get { 
+                return name;
+            }
+            set {
+                name = value; 
+            }
+            
+        }
+        
+        public int Age { get => age;
+
+
+            set
+            {
+                if (value < 0)
+                {
+                    age = 0;
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+}
